2a2dce701896f3bfce4e45d3eb2e944097da777c
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-11-07 02:35:49 -0300

    Testes rota Login e Teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -12,34 +12,121 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+const loginOk= {
+  email: 'teste@teste.com',
+  password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc', 
 
-  // let chaiHttpResponse: Response;
+}
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+const loginFailnoEmail = {
+  email: '',
+  password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc', 
+}
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+const loginFailnoPass = {
+  email: 'teste@teste.com',
+  password: '',
+}
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+const teams = 
+  [
+    {
+      id: 1,
+      teamName: 'Avaí/Kindermann',
+    },
+    {
+      id: 2,
+      teamName: 'Bahia',
+    },
+    {
+      id: 3,
+      teamName: 'Botafogo',
+    },
+    {
+      id: 4,
+      teamName: 'Corinthians',
+    },
+    {
+      id: 5,
+      teamName: 'Cruzeiro',
+    },
+    {
+      id: 6,
+      teamName: 'Ferroviária',
+    },
+    {
+      id: 7,
+      teamName: 'Flamengo',
+    },
+    {
+      id: 8,
+      teamName: 'Grêmio',
+    },
+    {
+      id: 9,
+      teamName: 'Internacional',
+    },
+    {
+      id: 10,
+      teamName: 'Minas Brasília',
+    },
+    {
+      id: 11,
+      teamName: 'Napoli-SC',
+    },
+    {
+      id: 12,
+      teamName: 'Palmeiras',
+    },
+    {
+      id: 13,
+      teamName: 'Real Brasília',
+    },
+    {
+      id: 14,
+      teamName: 'Santos',
+    },
+    {
+      id: 15,
+      teamName: 'São José-SP',
+    },
+    {
+      id: 16,
+      teamName: 'São Paulo',
+    },
+  ]
 
-  //   expect(...)
-  // });
+  const oneTeam = {
+    id: 16,
+    teamName: 'São Paulo',
+  }
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+
+describe('Testa rota Login', () => {
+  it('Testa Login corretamente', async () => {
+    const teste = await chai.request(app).post('/login').send(loginOk);
+    expect(teste.status).to.be.equal(200);
   });
+  it('Testa Login sem email', async () => {
+    const teste = await chai.request(app).post('/login').send(loginFailnoEmail);
+    expect(teste.status).to.be.equal(200);
+  });
+  it('Testa Login sem senha', async () => {
+    const teste = await chai.request(app).post('/login').send(loginFailnoPass);
+    expect(teste.status).to.be.equal(200);
+  });
+});
+
+describe('Testa rota Teams', () => {
+  it('Testa retorno de todos os times', async () => {
+    const teste = await chai.request(app).get('/teams').send();
+    expect(teste.body).to.deep.equal(teams);
+  });
+  it('Testa retorno de todos os times', async () => {
+    const teste = await chai.request(app).get('/teams/16').send();
+    expect(teste.body).to.deep.equal(oneTeam);
+  });
+
+
+
 });
